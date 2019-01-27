61ebf48b017532dc0e50a9b3411a7f321f9d69c8
jdlima <joao.diego.r.lima@gmail.com>
2019-01-27 16:58:36 -0200

    add

---
+++ features/step_definitions/get_emprestimo_steps.rb
@@ -0,0 +1,15 @@
+Dado("que o cliente tem um emprestimo criado") do
+    #pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Dado("possui um token valido para se comunicar com o servico") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+
+  Quando("o client consultar o servico de emprestimo informando o ID do emprestimo") do
+    #pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Entao("o servico deve me retornar as informacoes do emprestimo criado") do
+    #pending # Write code here that turns the phrase above into concrete actions
+  end
